7179348c1e8c264b0514e3c884f955d689f90835
Suke0811 <yusuketanaka@g.ucla.edu>
2021-08-26 13:11:32 -0700

    Added paperbot generation code, webots world

---
+++ TestPaperbot.py
@@ -0,0 +1,21 @@
+from rocolib.library import getComponent
+
+length = 200
+width = 130
+height = 60
+
+a = getComponent("Paperbot", length=length, width=width, height=height)
+dim = {"length": length, "width": width, "height": height}
+a.makeOutput("output/paperbot", thickness=5, tree=False, display=False, Webots=True, Dimensions=dim)
+
+
+# for length in range(80,200,50):
+#     for width in range(60,200,50):
+#         for height in range(20,80,50):
+#             a=None
+#             a = getComponent("Paperbot", length=length, width=width, height=height)
+#             a.makeOutput("output/Paperbot/%(length)d_%(width)d_%(height)d/" %locals(), thickness=5, tree=False, display=False, Webots=False)
+
+
+
+
---
+++ webots/controllers/scenic_supervisor/test.scenic
@@ -0,0 +1,12 @@
+workspace = Workspace(RectangularRegion(0 @ 0, 0, 2, 2))
+
+ego = Object at 0 @ -0.8, facing Range(-45, 45) deg,
+  with width 0.1, with length 0.1,
+  with webotsType 'Car'
+
+target = Object at 0 @ 0.8,
+  with width 0.01, with length 0.01,
+  with webotsType 'Target'
+  
+Object with webotsType 'Hill',
+  with width 2, with length 1, with height 0.2
